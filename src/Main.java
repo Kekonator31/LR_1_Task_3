3c6d0c5b0f6a99b71e3b150d83d4860e79677fae
Kekonator31 <48918994+Kekonator31@users.noreply.github.com>
2025-03-24 21:51:57 +0200

    Normal Version

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+class UserAccount{
+    private String password = "password";
+
+    void changePassword(String oldPassword, String newPassword){
+        if(oldPassword.equals(password)){
+            password = newPassword;
+            System.out.println("Password changed");
+        }
+        else{
+            System.out.println("Wrong old password");
+        }
+    }
+
+    void validatePassword(String password){
+        if(password.equals(this.password)){
+            System.out.println("Password \"" + password + "\" valid");
+        }
+        else{
+            System.out.println("Entered wrong password");
+        }
+    }
+
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserAccount user = new UserAccount();
+
+        user.validatePassword("password");
+        user.changePassword("Password", "THEPassword");
+        user.changePassword("password", "THEPassword");
+        user.validatePassword("tHEPassword");
+
+    }
+}
